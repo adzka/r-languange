e80fbe374ed91ae6c4c86f7cdeb04086c16cdda1
adzka <43627988+adzka@users.noreply.github.com>
2018-12-16 22:37:23 +0700

    Power Method Using Library Matlib

---
+++ power_method.r
@@ -0,0 +1,16 @@
+#no1
+a <- matrix(c(4,-5,2,3), 2,2, byrow = TRUE)
+a
+
+library(matlib)
+powerMethod(a)
+
+
+
+#no2
+b <- matrix(c(0,11,-5,-2,17,-7,-4,26,-10), 3,3, byrow = TRUE)
+b
+
+library(matlib)
+powerMethod(b)
+
